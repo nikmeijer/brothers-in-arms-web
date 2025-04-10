9a26991ef3e72fa4c8847f0e25b88d22daa4713a
nikmeijer <n_meijer@fanshaweonline.ca>
2025-04-10 05:50:47 -0400

    mass debug and detail
    With the care I give my car, I went through and gave this baby a full interior, exterior, and rim polish with some tire shine and ceramic coating to boot. (I need sleep)
    
    1 - Fixed nav donate now links with temporary placeholder redirect to contact
    2 - Deleted timeline.html; not needed, timeline on history page now, same with crosses page.
    3 - checked for and added latest and beautifullest footer version to all pages
    4 - Give as you live links filled out
    5 - got rid of nav background, too intrusive on visual flow of site, added white backgrounds to about page mission and park sections for better
    6 -  uxunify button styles, consistency is key
    7 - Fixed contact page alignment
    8 - added linkable email and phone number on contact page body text
    9 - overall content styling finished. :) YAY
    
    NEXT STEP:
    add images

--- contact.php
+++ contact.php
@@ -19,7 +19,7 @@
 <body>
     <h1 class="hidden">Brothers in Arms</h1>
 
-    <header class="full-width-grid-con">
+    <header class="full-width-grid-con real-header">
         <h2 class="hidden">Header</h2>
         <div id="logo" class="col-start-2 col-end-2">
             <a href="index.html">
@@ -31,10 +31,11 @@
             <li><a href="index.html">Home</a></li>
             <li><a href="about.html">About</a></li>
             <li><a href="history.html">History</a></li>
+            <li><a href="news.html">News</a></li>
             <li><a href="event.html">Events</a></li>
-            <li><a href="donations.html">Donations</a></li>
-            <li><a href="contact.html">Contact</a></li>
-            <li><a href="*" class="donate-nav">Donate Now</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="contact.php" class="see-all-button m-col-start-6 m-col-end-8">Donate Now</a></li>
+            <!--temporary placeholder link redirect to contact page, anyone with questions and some critical thinking skills who are really interested will reach out to find out until proper link is set up-->
         </nav>
         <!-- Mobile Menu -->
         <div id="mobile-nav" class="col-start-3 col-end-5 mobile">
@@ -46,10 +47,10 @@
                     <li><a href="index.html">Home</a></li>
                     <li><a href="about.html">About</a></li>
                     <li><a href="history.html">History</a></li>
+                    <li><a href="news.html">News</a></li>
                     <li><a href="event.html">Events</a></li>
-                    <li><a href="donations.html">Donations</a></li>
-                    <li><a href="contact.html">Contact</a></li>
-                    <li><a href="*" class="donate-nav">Donate Now</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="contact.php" class="see-all-button m-col-start-6 m-col-end-8">Donate Now</a></li>
                 </ul>
             </div>
         </div>
@@ -62,28 +63,32 @@
                 <p class="col-span-4 m-col-span-12 l-col-span-12 xl-col-span-12">Get in touch with us! If you have any
                     questions, suggestions, or would like to learn more about the Brothers in Arms Memorial, feel free
                     to
-                    reach out. You can contact us via email at info@brothersinarmsmemorial.org or call us at (555)
-                    123-4567.
-                    We look forward to hearing from you!</p>
+                    reach out. You can contact us via email at<a
+                        href="mailto:info@brothersinarmsmemorial.org">info@brothersinarmsmemorial.org</a>or call us at<a
+                        href="tel:+15551234567">(555)
+                        123-4567</a>.
+                    We look forward to hearing from you!
+                </p>
             </div>
         </section>
 
         <section class="grid-con" id="contact-con">
-            <div class="col-span-4 m-col-span-7 l-col-span-7 xl-col-span-7" id="contact-text">
+            <div class="col-span-4 m-col-span-6 l-col-span-6 xl-col-span-6" id="contact-text">
                 <h2>Make a Donation</h2>
                 <p>Help us achieve our goal of immortalizing those who fought and served in the Great War</p>
                 <h3>$47,894 / $200,000</h3>
                 <p>Raised so far</p>
                 <button><a href="*">Donate Now</a></button>
                 <h2>Give as you Live</h2>
-                <p>We are registered with <a href="*">Give as you Live.</a> This means we will receive a small donation
+                <p>We are registered with <a href="https://www.giveasyoulive.com/">Give as you Live.</a> This means we
+                    will receive a small donation
                     if
-                    you do your online shopping via Give as you Live. You can sign up to support us via <a href="*">this
+                    you do your online shopping via Give as you Live. You can sign up to support us via <a
+                        href="https://www.giveasyoulive.com/">this
                         link</a>.</p>
             </div>
 
-            <div id="contact"
-                class="col-span-4 m-col-span-5 m-col-start-8 l-col-span-5 l-col-start-8 xl-col-span-5 xl-col-start-8">
+            <div id="contact" class="col-span-4 m-col-span-6 l-col-span-6 xl-col-span-6">
                 <form method="post" action="includes/sendmail.php">
                     <label for="name">Name:</label>
                     <input type="text" id="name" name="name" required>
@@ -107,9 +112,45 @@
         </section>
     </main>
 
-    <footer>
+    <div id="site-footer">
+        <footer class="grid-con">
+            <div class="col-span-4 m-col-start-1 m-col-end-5 l-col-start-1 l-col-end-5" id="footer-logo">
+                <a href="index.html">
+                    <img src="images/BIA_LOGO_COLOUR.png" alt="Logo">
+                </a>
+            </div>
 
-    </footer>
+
+            <div class="col-span-2 m-col-start-1 m-col-end-5  footer-container" id="footer-nav-main-container">
+                <ul id="footer-nav-main">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="history.html">History</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+
+            <div class="col-span-2 m-col-start-6 m-col-end-9 footer-container" id="footer-nav-secondary-container">
+                <ul id="footer-nav-secondary">
+                    <li><a href="*">Terms & Conditions</a></li>
+                    <li><a href="*">Sitemap</a></li>
+                    <li><a href="*">Donate</a></li>
+                </ul>
+            </div>
+
+            <div class="col-span-4 m-col-span-4 l-col-span-4 xl-col-span-4 footer-container"
+                id="footer-subscribe-container">
+                <div id="footer-subscribe">
+                    <form method="post" action="*">
+                        <input type="email" id="newsletter" name="newsletter" placeholder="Subscribe to our newsletter!"
+                            required>
+                        <button type="submit">Subscribe</button>
+                    </form>
+                </div>
+            </div>
+
+        </footer>
+    </div>
 </body>
 
 </html>
